0cb43d5ec075f582dc5f65730372df592a5b2b52
Len Smith <len@barrison.com>
2016-11-29 13:57:27 -0800

    Started Slides

--- presentation/index.js
+++ presentation/index.js
@@ -36,135 +36,244 @@ require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
 
 
+String.prototype.fmt = function() {
+  return this.replace("/", "")
+};
+
 const images = {
   city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+  react: require("../assets/react.png"),
+  chrome: require("../assets/chrome.png"),
+  android: require("../assets/android.png"),
+  ios: require("../assets/ios.png"),
+  codemash: require("../assets/codemash.gif")
 };
 
 preloader(images);
 
 const theme = createTheme({
-  primary: "#ff4081"
+  primary: "#014081",
+  secondary: "white"
 });
+console.log("theme", theme);
 
+const imageWidth = 90
+
+class Exercise extends React.Component {
+  render() {
+    return(
+      <div>
+        <Heading>Exercise</Heading>
+        <Image src={images.codemash.fmt()}/>
+
+        <Text>{this.props.title}</Text>
+
+        <CodePane
+          textSize={this.props.textSize || "28px"}
+          margin="30px"
+          source={this.props.source}
+        />
+      </div>
+      )
+    }
+}
 export default class Presentation extends React.Component {
   render() {
     return (
       <Spectacle theme={theme}>
         <Deck transition={["zoom", "slide"]} transitionDuration={500}>
+
           <Slide transition={["zoom"]} bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
-            </Heading>
+
+            <Heading size={1} fit caps lineHeight={1} textColor="white"> React Everywhere </Heading>
+
+            <Image height={imageWidth} width={imageWidth} src={images.chrome.fmt()}/>
+            <Image height={imageWidth} width={imageWidth} src={images.android.fmt()}/>
+            <Image height={imageWidth} width={imageWidth} src={images.ios.fmt()}/>
+
             <Heading size={1} fit caps>
-              A ReactJS Presentation Library
+              Writing React for Web, android and iOS
             </Heading>
-            <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
-            </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
-              <Text bold caps textColor="tertiary">View on Github</Text>
+
+            <Link href="https://twitter.com/ignu">
+              <Text bold caps textColor="tertiary">Len Smith - @ignu</Text>
             </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
           </Slide>
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
-            </Heading>
+
+          <Slide bgImage={images.react.fmt()} bgDarken={0.75} transition={["zoom", "fade"]}>
+             <Heading fit caps>Why?</Heading>
           </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
+
+          <Slide bgColor="primary" notes="">
+            <Heading fit caps>Component Based</Heading>
+
             <CodePane
+              textSize="32"
               lang="jsx"
               source={require("raw!../assets/deck.example")}
-              margin="20px auto"
+              margin="19px auto"
             />
           </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-            <Appear fid="1">
-              <Heading size={1} caps fit textColor="primary">
-                Full Width
-              </Heading>
-            </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
-            </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
-            </Appear>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Flexible Layouts</Heading>
-            <Layout>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Left
-                </Heading>
-              </Fill>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Right
-                </Heading>
-              </Fill>
-            </Layout>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black">
-            <BlockQuote>
-              <Quote>Wonderfully formatted quotes</Quote>
-              <Cite>Ken Wheeler</Cite>
-            </BlockQuote>
-          </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
-            </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
 
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And lets not forget **bold**
-              `}
-            </Markdown>
+          <Slide bgColor="primary" notes="">
+            <Heading caps>JSX</Heading>
+
+            <CodePane
+              textSize="30"
+              lang="jsx"
+              source={require("raw!../assets/nested.example")}
+              margin="19px auto"
+            />
           </Slide>
-          <Slide transition={["slide", "spin"]} bgColor="primary">
-            <Heading caps fit size={1} textColor="tertiary">
-              Smooth
-            </Heading>
-            <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
-            </Heading>
+
+          <Slide bgColor="primary" notes="">
+            <Heading fit caps>Even this deck...</Heading>
+
+            <CodePane
+              textSize="30px"
+              lang="jsx"
+              source={require("raw!../assets/slide.example")}
+              margin="19px auto"
+            />
           </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>React-Router navigation</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
-            </List>
+
+          <Slide bgImage={images.react.fmt()} bgDarken={0.75} transition={["zoom", "fade"]}>
+             <Heading fit caps>Batteries not included</Heading>
+             <List>
+               <ListItem>Babel (JSX/ES6)</ListItem>
+               <ListItem>Webpack</ListItem>
+               <ListItem>Linting</ListItem>
+               <ListItem>Testing</ListItem>
+               <ListItem>Hot Reloading</ListItem>
+               <ListItem>Dev Tools</ListItem>
+             </List>
           </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
-            </Heading>
-            <Interactive/>
+
+          <Slide>
+            <Exercise
+              title="Make a session component that takes props"
+              source="<Session title='React Everywhere' description='...'/>"
+            />
           </Slide>
-          <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
-            </Heading>
-            <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
+
+          <Slide>
+            <Exercise
+              title="Nest a Speaker component inside of Session"
+              source="<Speaker name={firstName + lastName} />"
+            />
           </Slide>
+
+          <Slide>
+            <Exercise
+              title="Buid and deploy our app"
+              source={ `> npm run build
+> npm install surge -g && surge public`}
+            />
+          </Slide>
+
+          <Slide>
+            <Exercise
+              title="make this a task in package.json"
+              source="deploy: npm run build && surge public"
+            />
+          </Slide>
+
+          <Slide bgColor="primary" notes="">
+            <Heading fit caps>Testing...</Heading>
+          </Slide>
+
+          <Slide>
+            <Exercise
+              title="Write Smoke Tests for Session/Speaker"
+              source={`it('renders without crashing', () => {`}
+            />
+          </Slide>
+
+          <Slide bgColor="primary" notes="">
+            <Heading fit caps>Stateless Functional Component</Heading>
+            <CodePane
+              margin="30px"
+              source={`(props) => <j`}
+            />
+          </Slide>
+
+          <Slide>
+            <Exercise
+              title="Refactor Speaker to functional component"
+              source={`it('renders without crashing', () => {`}
+            />
+          </Slide>
+
+          <Slide>
+            <Exercise
+              title="Set up a failing unit tests for requiring propTypes"
+              source={`it("requires a firstName prop") {
+  shallow(<Session />)
+  expect(spy.calledOnce()).to.equal(true)
+}
+`}
+            />
+          </Slide>
+
+          <Slide bgColor="primary" notes="">
+            <Heading fit caps>Fetch</Heading>
+          </Slide>
+
+          <Slide>
+            <Exercise
+              title="Fetch Session data from CodeMash api"
+              source="<Speaker name={firstName + lastName} />"
+            />
+          </Slide>
+
+          <Slide bgColor="primary" notes="">
+            <Heading fit caps>Children</Heading>
+          </Slide>
+
+          <Slide>
+            <Exercise
+              title="Create Loading Comopnent"
+      source={ `
+        <Loading loading={this.state.loading}>
+          <SessionList sessions={this.state.sessions}/>
+        </Loading>
+` } />
+          </Slide>
+
+          <Slide bgColor="primary" notes="">
+            <Heading fit caps>Events</Heading>
+          </Slide>
+
+          <Slide>
+            <Exercise
+              title="Select an Event"
+              source="<Session onSelect={this.select.bind(this)} />"
+            />
+          </Slide>
+
+          <Slide bgColor="primary" notes="">
+            <Heading fit caps>git co master</Heading>
+          </Slide>
+
+          <Slide bgColor="primary" notes="">
+            <Heading fit caps>Redux</Heading>
+          </Slide>
+
+          <Slide>
+            <Exercise
+              title="Put user data in "
+              source="<Speaker name={firstName + lastName} />"
+            />
+          </Slide>
+
+          <Slide bgColor="primary" notes="">
+            <Heading fit caps>Router</Heading>
+          </Slide>
+
+          <Slide bgColor="primary" notes="">
+            <Heading fit caps>Ramda</Heading>
+          </Slide>
+
         </Deck>
       </Spectacle>
     );
